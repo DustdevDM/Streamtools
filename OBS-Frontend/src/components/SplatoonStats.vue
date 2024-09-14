05ff29f599da6ff6d49fd2210899780fc717af0f
Dustdev_DM <public@dustin-dm.de>
2024-09-14 15:44:20 +0200

    reinit project

---
+++ OBS-Frontend/src/components/SplatoonStats.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="SplatoonStats">
+    <h1>Splatoon Stats</h1>
+    <p>{{}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SplatoonStats'
+}
+</script>
